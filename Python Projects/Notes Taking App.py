ed7f37def45c96fc6feb19c953ed7ddfcab27ea5
SwetaReddy30 <sweta.reddy259@gmail.com>
2026-02-25 16:36:03 +0530

    Update Notes Taking App.py

--- Python Projects/Notes Taking App.py
+++ Python Projects/Notes Taking App.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title("Hello, Add your notes here 📝!")
-
+st.sidebar.title("Notes Taking App")
+st.sidebar.write("This is a simple notes taking app built with Streamlit. You can use it to jot down your thoughts, ideas, or anything you want to remember.")
