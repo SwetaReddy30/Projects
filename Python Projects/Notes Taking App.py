55188c0d772185cf1e0d5dd57db8357fa8774c68
SwetaReddy30 <sweta.reddy259@gmail.com>
2026-02-25 16:43:28 +0530

    Update Notes Taking App.py

--- Python Projects/Notes Taking App.py
+++ Python Projects/Notes Taking App.py
@@ -2,3 +2,14 @@ import streamlit as st
 st.title("👋 Hello, Add your notes here! 📝")
 st.sidebar.title("Notes Buddy 😎")
 st.sidebar.write("You can use me to jot down your thoughts, ideas, or anything you want to remember.")
+
+import streamlit as st
+import time
+
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
