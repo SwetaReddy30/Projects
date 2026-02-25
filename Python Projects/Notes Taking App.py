2bc30203d71941fb855c26cea014aa7c764f9978
SwetaReddy30 <sweta.reddy259@gmail.com>
2026-02-25 16:57:15 +0530

    Update Notes Taking App.py

--- Python Projects/Notes Taking App.py
+++ Python Projects/Notes Taking App.py
@@ -3,8 +3,7 @@ st.title("👋 Hello, Add your notes here! 📝")
 st.sidebar.title("Notes Buddy 😎")
 st.sidebar.write("You can use me to jot down your thoughts, ideas, or anything you want to remember.")
 with st.sidebar:
-    with st.echo():
-        st.write("➕ Add new notes below:")
-        st.write("🗒️ View Notes")
-        st.write("🔎 Search Notes")
-        st.write("📊 Analytics")
+    st.write("➕ Add new notes below:")
+    st.write("🗒️ View Notes")
+    st.write("🔎 Search Notes")
+    st.write("📊 Analytics")
